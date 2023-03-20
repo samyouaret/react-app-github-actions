dbc093e05ccfa6115180f830a6332b0e70b09fd9
samyouaret <samyouaret13@gmail.com>
2023-03-20 16:30:47 +0100

    Add github actions workflow

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+console.log(process.env.REACT_APP_MY_VAR);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{process.env.REACT_APP_MY_VAR}</h2>
       </header>
     </div>
   );
